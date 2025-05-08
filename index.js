4f1efb577fbf9257ec470becfe0074e52b1f5069
abdullahjim29 <abdullahjim2002@gmail.com>
2025-05-08 21:41:41 +0600

    server side initialized with installing needed tools

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+// create root api
+app.get('/', (req, res) => {
+    res.send('A 11 server running.....!')
+})
+
+
+app.listen(port, () => {
+    console.log(`server is running at post ${port}`);
+})
