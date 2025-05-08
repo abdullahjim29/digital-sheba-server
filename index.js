bafa493e7c642f2207894166f85c39e5c47e1ca6
abdullahjim29 <abdullahjim2002@gmail.com>
2025-05-08 22:21:16 +0600

    created two apis, one for get all services and second for add a single service

--- index.js
+++ index.js
@@ -1,10 +1,65 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+// mongodb connection
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.orsq4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // create collection
+    const serviceCollection = client.db('servicesDB').collection('services');
+
+
+    // all APIs
+
+
+    // get all services
+    app.get('/services', async (req, res) => {
+        const services = req.body;
+        const result = await serviceCollection.find(services).toArray();
+        res.send(result)
+    })
+
+
+    // add service to DB
+    app.post('/add-service', async (req, res) => {
+        const newService = req.body;
+        const result = await serviceCollection.insertOne(newService);
+        res.send(result);
+    })
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 // create root api
