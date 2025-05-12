75ae109cc4907441338fb39ac891ecb0e6ba3db9
abdullahjim29 <abdullahjim2002@gmail.com>
2025-05-12 12:41:17 +0600

    create an api to get a specific service

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -41,7 +41,8 @@ async function run() {
     // get all services
     app.get('/services', async (req, res) => {
         const services = req.body;
-        const result = await serviceCollection.find(services).toArray();
+        const limit = parseInt(req.query.limit) || 0;
+        const result = await serviceCollection.find(services).limit(limit).toArray();
         res.send(result)
     })
 
@@ -51,6 +52,15 @@ async function run() {
         const newService = req.body;
         const result = await serviceCollection.insertOne(newService);
         res.send(result);
+    })
+
+
+    // get a specific service
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
     })
 
   } finally {
